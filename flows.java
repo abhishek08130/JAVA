b9670b1d64a97e198cb631769c2d9d64139b034e
abhishek08130 <abhishekbhatt08130@gmail.com>
2025-02-05 11:33:37 +0530

    Number is greater than 4 or less than 4

---
+++ flows.java
@@ -0,0 +1,11 @@
+class Flows {
+    public static void main(String[] args) {
+        int number = 4;
+        if (number >= 4) {
+            System.out.println("number is greater than or equal to 4");
+        }
+        else {
+            System.out.println("number is less than 4");
+        }
+    }
+}
