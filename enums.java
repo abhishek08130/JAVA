2aa0bbc0124df3f90101c0434e5d9901a5be2bf5
abhishek08130 <abhishekbhatt08130@gmail.com>
2025-02-11 09:49:50 +0530

    enum days

---
+++ enums.java
@@ -0,0 +1,9 @@
+enum Day{
+    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+}
+public class enums {
+    public static void main(String[] args) {
+        Day today = Day.WEDNESDAY;
+        System.out.println("Today is " + today);
+    }
+}
