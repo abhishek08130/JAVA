2c849bb05f7d26daed38e0cd8b2930a3dbabdb3a
abhishek08130 <abhishekbhatt08130@gmail.com>
2025-02-17 11:36:50 +0530

    Constructor Overloading

--- constructor_Overloading.java
+++ constructor_Overloading.java
@@ -1,31 +1,15 @@
-public class constructor_Overloading {
-    private String b;
-    private int y;
+class constructor_Overloading{
+    String brand;
 
-    // Constructor with one parameter
-    public constructor_Overloading(String b) {
-        this.b = b;
-        this.y = 0; // default value
+    constructor_Overloading(){
+        System.out.println("Car is created");
+        brand = "Unknown";
     }
 
-    // Constructor with two parameters
-    public constructor_Overloading(String b, int y) {
-        this.b = b;
-        this.y = y;
-    }
-
-    // Method to display the values of b and y
-    public void display() {
-        System.out.println("Value of b: " + b + ", Value of y: " + y);
-    }
-
-    // Main method to demonstrate constructor overloading
-    public static void main(String[] args) {
-        constructor_Overloading obj1 = new constructor_Overloading("Hello");
-        constructor_Overloading obj2 = new constructor_Overloading("World", 10);
-
-        // Displaying the values for both objects
-        obj1.display(); // Output: Value of b: Hello, Value of y: 0
-        obj2.display(); // Output: Value of b: World, Value of y: 10
+    public class Main{
+        public static void main (String[] args){
+            constructor_Overloading myCar = new constructor_Overloading();
+            System.out.println(myCar.brand);
+        }
     }
 }
